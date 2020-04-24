10a5379260dc96d4bc40cbcdbc89df37ba0b3cb7
donchi4all <donchi4all@gmail.com>
2020-04-24 18:00:43 +0100

    transaction model

--- app/Product_Settings.php
+++ app/Product_Settings.php
@@ -6,4 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product_Settings extends Model
 {
+    public $fillable = [
+        'cart_vat',
+        'invoice_address'
+    ];
+
+    public static function settings(){
+        //  $settings=Setting::first();
+        //    return collect([
+        //     'tax' => $settings->cart_vat,
+        //     'address' => $settings->invoice_address,
+        // ]);
+           return collect([
+            'tax' => 300,
+            'address' => "demo Address",
+        ]);
+    }
 }
